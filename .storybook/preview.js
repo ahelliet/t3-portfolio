1b27175d7fe514f1e990c74e2b813cb9adbea5d1
Anthony Helliet <8291454-ahelliet@users.noreply.gitlab.com>
2022-11-14 12:06:30 +0100

    add nextRouter and a11y storybook addons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,21 +1,25 @@
-import "../src/styles/globals.css";
-import * as NextImage from "next/image";
+import '../src/styles/globals.css'
+import * as NextImage from 'next/image'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
 
 // Allow Storybook to handle Next's <Image> component
-const OriginalNextImage = NextImage.default;
+const OriginalNextImage = NextImage.default
 
-Object.defineProperty(NextImage, "default", {
+Object.defineProperty(NextImage, 'default', {
   configurable: true,
   value: (props) => <OriginalNextImage {...props} unoptimized />,
-});
+})
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-  layout: "centered",
-};
+  layout: 'centered',
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+}
